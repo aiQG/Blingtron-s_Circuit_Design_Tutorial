bb0e6ef1a65d1f4f1093a18d6be185cac3ff6076
aiQG_ <lovequange1314@vip.qq.com>
2020-01-05 14:15:12 +0800

    Initial Commit

---
+++ Blingtron-s_Circuit_Design_Tutorial/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Blingtron-s_Circuit_Design_Tutorial
+//
+//  Created by 周测 on 1/5/20.
+//  Copyright © 2020 aiQG_. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
