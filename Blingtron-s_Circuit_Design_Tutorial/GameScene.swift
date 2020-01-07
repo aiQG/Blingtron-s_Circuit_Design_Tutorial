ee2b675d9382e953b3e6a637eb9a9154a861b48a
aiQG_ <lovequange1314@vip.qq.com>
2020-01-07 13:08:33 +0800

    select to change color (developing)

--- Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
+++ Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
@@ -13,28 +13,33 @@ class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+	private var unSelectedPointColor: SKKeyframeSequence?
+	private var SelectedPointColor: SKKeyframeSequence? = nil
+	
     override func didMove(to view: SKView) {
-        let expnode = SKEmitterNode(fileNamed: "Point")!
-		expnode.position = CGPoint(x: 100, y: 100)
-		expnode.name = "testPoint"
-		addChild(expnode)
 		
+        let expnode0 = SKEmitterNode(fileNamed: "Point")!
+		expnode0.position = CGPoint(x: 100, y: 100)
+		expnode0.name = "testPoint0"
+		addChild(expnode0)
+		
+		let expnode1 = SKEmitterNode(fileNamed: "Point")!
+		expnode1.position = CGPoint(x: 50, y: 50)
+		expnode1.name = "testPoint1"
+		addChild(expnode1)
+
+		unSelectedPointColor = expnode0.particleColorSequence
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
 
     override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-//		print(event.allTouches().first!)
-		print(self.nodes(at: event.location(in: self)))
+		
+		guard let beSelectedPoint = self.nodes(at: event.location(in: self)).first as? SKEmitterNode else {
+			return
+		}
+
+		beSelectedPoint.particleColorSequence = beSelectedPoint.particleColorSequence == SelectedPointColor ? unSelectedPointColor : SelectedPointColor
+		
     }
     
     
