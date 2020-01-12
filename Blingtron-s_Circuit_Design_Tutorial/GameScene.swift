41521b222efe48fb482461a4874bd1e137fb8fbb
aiQG_ <lovequange1314@vip.qq.com>
2020-01-12 16:29:19 +0800

    swap function

--- Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
+++ Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
@@ -9,12 +9,15 @@
 import SpriteKit
 import GameplayKit
 
+
 class GameScene: SKScene {
     
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-	private var unSelectedPointColor: SKKeyframeSequence?
+	private var unSelectedPointColor: SKKeyframeSequence? = nil
 	private var SelectedPointColor: SKKeyframeSequence? = nil
+	private var firstPoint: SKEmitterNode? = nil
+	private var secondPoint: SKEmitterNode? = nil
 	
     override func didMove(to view: SKView) {
 		
@@ -27,6 +30,11 @@ class GameScene: SKScene {
 		expnode1.position = CGPoint(x: 50, y: 50)
 		expnode1.name = "testPoint1"
 		addChild(expnode1)
+		
+		let expnode2 = SKEmitterNode(fileNamed: "Point")!
+		expnode2.position = CGPoint(x: 150, y: 50)
+		expnode2.name = "testPoint2"
+		addChild(expnode2)
 
 		unSelectedPointColor = expnode0.particleColorSequence
     }
@@ -37,11 +45,36 @@ class GameScene: SKScene {
 		guard let beSelectedPoint = self.nodes(at: event.location(in: self)).first as? SKEmitterNode else {
 			return
 		}
-
+		
 		beSelectedPoint.particleColorSequence = beSelectedPoint.particleColorSequence == SelectedPointColor ? unSelectedPointColor : SelectedPointColor
+		
+		if firstPoint == nil && beSelectedPoint.particleColorSequence == SelectedPointColor  {
+			firstPoint = beSelectedPoint
+		} else if firstPoint == beSelectedPoint {
+			firstPoint = nil
+		} else if firstPoint != beSelectedPoint {
+			if beSelectedPoint.particleColorSequence == SelectedPointColor {
+				pointSwap(point1: firstPoint!, point2: beSelectedPoint)
+			}
+		}
+		
 		
     }
     
+	func pointSwap(point1:SKEmitterNode, point2:SKEmitterNode) -> Void {
+		print("swap")
+		var temp = point1.position.x
+		point1.position.x = point2.position.x
+		point2.position.x = temp
+		
+		temp = point1.position.y
+		point1.position.y = point2.position.y
+		point2.position.y = temp
+		
+		point1.particleColorSequence = unSelectedPointColor
+		point2.particleColorSequence = unSelectedPointColor
+		firstPoint = nil
+	}
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
