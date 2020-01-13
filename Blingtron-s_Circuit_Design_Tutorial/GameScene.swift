eac3f06ffc02f56c7309e2fdca231eaaf81e8069
aiQG_ <lovequange1314@vip.qq.com>
2020-01-13 16:25:17 +0800

    swap animation

--- Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
+++ Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
@@ -12,8 +12,6 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
 	private var unSelectedPointColor: SKKeyframeSequence? = nil
 	private var SelectedPointColor: SKKeyframeSequence? = nil
 	private var firstPoint: SKEmitterNode? = nil
@@ -22,20 +20,29 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
 		
         let expnode0 = SKEmitterNode(fileNamed: "Point")!
-		expnode0.position = CGPoint(x: 100, y: 100)
+		expnode0.position = CGPoint(x: 50, y: 50)
+		expnode0.zPosition = 1
 		expnode0.name = "testPoint0"
 		addChild(expnode0)
 		
 		let expnode1 = SKEmitterNode(fileNamed: "Point")!
-		expnode1.position = CGPoint(x: 50, y: 50)
+		expnode1.position = CGPoint(x: 100, y: 50)
+		expnode1.zPosition = 1
 		expnode1.name = "testPoint1"
 		addChild(expnode1)
 		
 		let expnode2 = SKEmitterNode(fileNamed: "Point")!
-		expnode2.position = CGPoint(x: 150, y: 50)
+		expnode2.position = CGPoint(x: 100, y: 100)
+		expnode2.zPosition = 1
 		expnode2.name = "testPoint2"
 		addChild(expnode2)
-
+		
+		let expnode3 = SKEmitterNode(fileNamed: "Point")!
+		expnode3.position = CGPoint(x: 50, y: 100)
+		expnode3.zPosition = 1
+		expnode3.name = "testPoint3"
+		addChild(expnode3)
+		
 		unSelectedPointColor = expnode0.particleColorSequence
     }
     
@@ -62,21 +69,22 @@ class GameScene: SKScene {
     }
     
 	func pointSwap(point1:SKEmitterNode, point2:SKEmitterNode) -> Void {
-		print("swap")
-		var temp = point1.position.x
-		point1.position.x = point2.position.x
-		point2.position.x = temp
-		
-		temp = point1.position.y
-		point1.position.y = point2.position.y
-		point2.position.y = temp
+		print("swap: \(point1) \nand\n \(point2)")
+		let point1Position = SKAction.move(to: CGPoint(x: point1.position.x, y: point1.position.y), duration: 0.5)
+		let point2Position = SKAction.move(to: CGPoint(x: point2.position.x, y: point2.position.y), duration: 0.5)
+
+		point1.run(point2Position)
+		point2.run(point1Position)
 		
 		point1.particleColorSequence = unSelectedPointColor
 		point2.particleColorSequence = unSelectedPointColor
 		firstPoint = nil
+		
+
 	}
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+		
     }
 }
