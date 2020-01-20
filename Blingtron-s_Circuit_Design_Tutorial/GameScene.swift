e316ba3bc2ca5d201540ce6edb0217a1c6246aea
aiQG_ <lovequange1314@vip.qq.com>
2020-01-20 16:48:06 +0800

    线的变化完成, 缺少线的动画

--- Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
+++ Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
@@ -17,40 +17,40 @@ class GameScene: SKScene {
 	private var firstPoint: SKEmitterNode? = nil
 	private var secondPoint: SKEmitterNode? = nil
 	
+	var pointList = [
+		CGPoint(x: -50, y: -50),
+		CGPoint(x: -50, y:  50),
+		CGPoint(x:  50, y:  50),
+		CGPoint(x:  50, y: -50)
+	]
 	
 	var skShape:SKShapeNode?
 	var linePath:CGMutablePath?
 	
+	var tryLine:SKShapeNode?
+	var tryPath0:CGMutablePath?
+	var tryPath1:CGMutablePath?
+	
+	
+	
     override func didMove(to view: SKView) {
 		
 		skShape = SKShapeNode()
-		
-		
+		pointList.append(pointList[0])//最后一个为第0个, 用于封闭图形
 		linePath = CGMutablePath()
-		linePath?.addLines(
-			between: [CGPoint(x: -50, y: -50),
-					  CGPoint(x: -50, y: 50),
-					  CGPoint(x: 50, y: 50),
-					  CGPoint(x: 50, y: -50),
-					  CGPoint(x: -50, y: -50)]	//最后一个为第0个, 用于封闭图形
-		)
+		linePath?.addLines(between: pointList)
 		skShape?.path = linePath
 		skShape?.strokeColor = .red
 		addChild(skShape!)
 		
-		for index in 0...(skShape?.path!.points)!.count-2 {
+		for index in 0...pointList.count-2 {
 			let node = SKEmitterNode(fileNamed: "Point")
-			node?.position = (skShape?.path!.points)![index]
+			node?.position = pointList[index]
 			node?.zPosition = 1
 			node?.name = "testPt\(index)"
 			skShape!.addChild(node!)
 			unSelectedPointColor = node!.particleColorSequence
 		}
-		
-		
-		
-		print(skShape?.path?.points)
-		
 		
     }
     
@@ -72,12 +72,10 @@ class GameScene: SKScene {
 				pointSwap(point1: firstPoint!, point2: beSelectedPoint)
 			}
 		}
-		
-		
     }
     
 	func pointSwap(point1:SKEmitterNode, point2:SKEmitterNode) -> Void {
-		print("swap: \(point1) \nand\n \(point2)")
+		//points
 		let point1Position = SKAction.move(
 			to: CGPoint(x: point1.position.x, y: point1.position.y),
 			duration: 0.5)
@@ -87,49 +85,58 @@ class GameScene: SKScene {
 		point1.run(point2Position)
 		point2.run(point1Position)
 		
-		//line
-		
-		print(skShape)
-		
-		
-		
 		point1.particleColorSequence = unSelectedPointColor
 		point2.particleColorSequence = unSelectedPointColor
 		firstPoint = nil
+		
+		//lines
+		print(pointList)
+		pointList.swapAt(
+			pointList.firstIndex(of: point1.position)!,
+			pointList.firstIndex(of: point2.position)!
+		)
+		pointList.remove(at: pointList.count-1)
+		pointList.append(pointList[0])
+		print(pointList)
+		
 	}
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-		
+		removeChildren(in: [skShape!])
+		linePath = CGMutablePath()
+		linePath?.addLines(between: pointList)
+		skShape?.path = linePath
+		addChild(skShape!)
     }
 }
 
 //导出CGPath的点数组
-extension CGPath {
-  var points: [CGPoint] {
-
-     var arrPoints: [CGPoint] = []
-
-     self.applyWithBlock { element in
-        switch element.pointee.type
-        {
-        case .moveToPoint, .addLineToPoint:
-          arrPoints.append(element.pointee.points.pointee)
-
-        case .addQuadCurveToPoint:
-          arrPoints.append(element.pointee.points.pointee)
-          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
-
-        case .addCurveToPoint:
-          arrPoints.append(element.pointee.points.pointee)
-          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
-          arrPoints.append(element.pointee.points.advanced(by: 2).pointee)
-
-        default:
-          break
-        }
-     }
-
-    return arrPoints
-  }
-}
+//extension CGPath {
+//  var points: [CGPoint] {
+//
+//     var arrPoints: [CGPoint] = []
+//
+//     self.applyWithBlock { element in
+//        switch element.pointee.type
+//        {
+//        case .moveToPoint, .addLineToPoint:
+//          arrPoints.append(element.pointee.points.pointee)
+//
+//        case .addQuadCurveToPoint:
+//          arrPoints.append(element.pointee.points.pointee)
+//          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
+//
+//        case .addCurveToPoint:
+//          arrPoints.append(element.pointee.points.pointee)
+//          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
+//          arrPoints.append(element.pointee.points.advanced(by: 2).pointee)
+//
+//        default:
+//          break
+//        }
+//     }
+//
+//    return arrPoints
+//  }
+//}
