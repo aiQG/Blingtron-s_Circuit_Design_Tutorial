11cf80db11cb5865eec97ed7fcf9c3136e34a8af
aiQG_ <lovequange1314@vip.qq.com>
2020-02-11 14:16:14 +0800

    同一线上的点交换,线始终连接.✅

--- Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
+++ Blingtron-s_Circuit_Design_Tutorial/GameScene.swift
@@ -34,17 +34,30 @@ class GameScene: SKScene {
 	
 	
     override func didMove(to view: SKView) {
-		
-		pointList.append(pointList[0])//最后一个为第0个, 用于封闭图形
-		
-		for index in 0...pointList.count-2 {
+		// 创建点
+		for index in 0...pointList.count-1 {
 			let node = SKEmitterNode(fileNamed: "Point")
 			node?.position = pointList[index]
 			node?.zPosition = 1
-			node?.name = "testPt\(index)"
+			node?.name = "\(index)"
 			addChild(node!)
 			unSelectedPointColor = node!.particleColorSequence
 		}
+		
+		// 遍历点 并连起
+		let pA = childNode(withName: "2")
+		let pB = childNode(withName: "0")
+		
+		let line = SKShapeNode()
+		let path = CGMutablePath()
+		path.move(to: pB!.position)
+		path.addLine(to: pA!.position)
+		line.path = path
+		line.strokeColor = .red
+		line.name = "0-2"
+		addChild(line)
+		
+//		print(path.points)
 		
     }
     
@@ -69,6 +82,17 @@ class GameScene: SKScene {
     }
     
 	func pointSwap(point1:SKEmitterNode, point2:SKEmitterNode) -> Void {
+		
+		//line
+		let line: SKShapeNode = (childNode(withName: "\(point1.name!)-\(point2.name!)") ?? childNode(withName: "\(point2.name!)-\(point1.name!)")) as! SKShapeNode
+		let topath = CGMutablePath()
+		topath.move(to: Int(point1.name!)! < Int(point2.name!)! ? point2.position : point1.position)
+		topath.addLine(to: Int(point1.name!)! > Int(point2.name!)! ? point2.position : point1.position)
+		print(topath)
+		print(line.path)
+		line.run(SKAction.lineAnim(fromPath: line.path!, toPath: topath, duration: 0.5))
+		
+		
 		//points
 		let point1Position = SKAction.move(
 			to: CGPoint(x: point1.position.x, y: point1.position.y),
@@ -91,31 +115,53 @@ class GameScene: SKScene {
 }
 
 //导出CGPath的点数组
-//extension CGPath {
-//  var points: [CGPoint] {
-//
-//     var arrPoints: [CGPoint] = []
-//
-//     self.applyWithBlock { element in
-//        switch element.pointee.type
-//        {
-//        case .moveToPoint, .addLineToPoint:
-//          arrPoints.append(element.pointee.points.pointee)
-//
-//        case .addQuadCurveToPoint:
-//          arrPoints.append(element.pointee.points.pointee)
-//          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
-//
-//        case .addCurveToPoint:
-//          arrPoints.append(element.pointee.points.pointee)
-//          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
-//          arrPoints.append(element.pointee.points.advanced(by: 2).pointee)
-//
-//        default:
-//          break
-//        }
-//     }
-//
-//    return arrPoints
-//  }
-//}
+extension CGPath {
+  var points: [CGPoint] {
+
+     var arrPoints: [CGPoint] = []
+
+     self.applyWithBlock { element in
+        switch element.pointee.type
+        {
+        case .moveToPoint, .addLineToPoint:
+          arrPoints.append(element.pointee.points.pointee)
+
+        case .addQuadCurveToPoint:
+          arrPoints.append(element.pointee.points.pointee)
+          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
+
+        case .addCurveToPoint:
+          arrPoints.append(element.pointee.points.pointee)
+          arrPoints.append(element.pointee.points.advanced(by: 1).pointee)
+          arrPoints.append(element.pointee.points.advanced(by: 2).pointee)
+
+        default:
+          break
+        }
+     }
+
+    return arrPoints
+  }
+}
+
+//线动画
+extension SKAction {
+    static func lineAnim(fromPath: CGPath, toPath: CGPath, duration: Double = 0.5) -> SKAction
+    {
+        return SKAction.customAction(withDuration: duration, actionBlock: { (node: SKNode!, elapsedTime: CGFloat) -> Void in
+            let fraction = CGFloat(elapsedTime / CGFloat(duration))
+            let start = fromPath
+            let end = toPath
+            let trans = CGMutablePath()
+			trans.move(to: CGPoint(
+				x: start.points.first!.x + (end.points.first!.x - start.points.first!.x) * fraction,
+				y: start.points.first!.y + (end.points.first!.y - start.points.first!.y) * fraction))
+			
+			trans.addLine(to: CGPoint(x: start.points.last!.x + (end.points.last!.x - start.points.last!.x) * fraction,
+									  y: start.points.last!.y + (end.points.last!.y - start.points.last!.y) * fraction))
+			
+			(node as? SKShapeNode)?.path = trans
+        }
+        )
+    }
+}
